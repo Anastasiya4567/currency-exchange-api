f6d85ded9a82162db4bebb92ffd23037707e6f75
Anastasiya4567 <302439@pw.edu.pl>
2026-04-26 16:01:01 +0200

    Initial setup

---
+++ src/main/java/com/nn/task/currency/exchange/api/CurrencyExchangeAPIApplication.java
@@ -0,0 +1,13 @@
+package com.nn.task.currency.exchange.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nn/task/currency/exchange/api/CurrencyExchangeAPIApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nn.task.currency.exchange.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfigurationTest.class)
+@SpringBootTest
+class CurrencyExchangeAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

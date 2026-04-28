347096f3d29bff1362804f52694ba18a80317f72
Anastasiya.Chaplianskaya <Anastasiya.Chaplianskaya@six-group.com>
2026-04-28 11:19:49 +0200

    Moved constant to application.yaml

--- src/main/java/com/nn/task/currency/exchange/api/client/NbpClient.java
+++ src/main/java/com/nn/task/currency/exchange/api/client/NbpClient.java
@@ -3,10 +3,9 @@ package com.nn.task.currency.exchange.api.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "nbpClient", url = "http://api.nbp.pl/api")
+@FeignClient(name = "nbpClient", url = "${nbp.api.url}")
 public interface NbpClient {
 
     @GetMapping("/exchangerates/rates/A/USD/?format=json")
     NbpRateResponse getUsdPlnRate();
 }
-
